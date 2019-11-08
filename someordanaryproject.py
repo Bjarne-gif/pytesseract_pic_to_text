9a80a2ebd1ce8d5130f80eaf5d8626acda38198f
Bjarne-gif <57255094+Bjarne-gif@users.noreply.github.com>
2019-11-08 22:51:49 +0100

    takeownpic
    added answer to take own pic

--- someordanaryproject.py
+++ someordanaryproject.py
@@ -29,19 +29,44 @@ def answerwantdelete():
         deletetxtfile()
         pass
     elif varanswerwantdelete == "n":
-        print("lasse Textdatei erstellt...")
+        print("lasse Textdatei erstellt und beende das script...")
         pass
     else:
         print("verstehe deine Antwort nicht, bitte schreibe y oder n")
         pass
 
+def answertotakeownpic():
+    varanswertotakeownpic = input("\n Beispieldatei benutzen? (text_bild2.PNG) y/n:")
+    if varanswertotakeownpic == "y":
+        writefile()
+        pass
+    elif varanswertotakeownpic == "n":
+        ownpic = input("\n Bitte Bild im gleichen Ordner legen und den Namen hier eintragen (Beispiel: Bild1.png):")
+        if not os.path.isfile(ownpic):
+            print("Deine Datei existiert nicht, ich nehme das Beispielfoto!")
+            writefile()
+            pass
+        elif os.path.isfile(ownpic):
+            print("Deine Datei wird ausgewertet und in die Textdatei geschrieben.")
+            img = ownpic
+            #writefile() -> Does not functioning because variables can only get into defs not out of defs
+            file = open(testtxt,"w") 
+            file.write(""+image_to_string(Image.open(img)))
+            file.close()   
+        else:
+            pass
+    else:
+        print("Ich verstehe deine Antwort nicht, ich nehme das Beispielfoto!")
+        writefile()
+
+
 #write text from the image into document or just read it
 if not os.path.isfile(testtxt):
+    answertotakeownpic()
     print("Textdatei wird erstellt und ausgegeben\n")
-    writefile()
     readfile()
 else:
-    print("Textdatei wurde nicht erstellt weil sie existiert und wird nun ausgelesen\n")
+    print("Textdatei existiert und wird nun ausgelesen, lösche sie um ggf. eine neue einzulesen\n")
     readfile()
 pass
 answerwantdelete() 
